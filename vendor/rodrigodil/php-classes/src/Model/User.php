442d59ca0138e90ed73176bf1d3447d5cc4c3451
Rodrigo Palombo Chisté <rodrigodil@live.com>
2018-11-02 13:15:27 -0300

    Tela de Login

---
+++ vendor/rodrigodil/php-classes/src/Model/User.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rodri
+ * Date: 02/11/2018
+ * Time: 11:54
+ */
+
+namespace rodrigodil\Model;
+
+use rodrigodil\DB\Sql;
+use rodrigodil\Model;
+
+
+class User extends Model {
+
+    const SESSION = "User";
+
+    public static function login ($login, $password)
+    {
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+            ":LOGIN"=>$login
+        ));
+
+        if (count($results) === 0)
+        {
+            throw new \Exception("Usuário não existe ou senha inválida.");
+        }
+
+        $data = $results[0];
+
+        if (password_verify($password, $data["despassword"]) === true)
+        {
+
+            $user = new User();
+
+            $user -> setData($data);
+
+            $_SESSION[User::SESSION] = $user -> getValues();
+
+            return $user;
+
+        } else {
+            throw new \Exception("Usuários não existe ou senha inválida.");
+        }
+    }
+
+    public static function verifyLogin($inadmin = true)
+    {
+
+        if (
+            !isset($_SESSION[User::SESSION])
+            ||
+            !$_SESSION[User::SESSION]
+            ||
+            !(int)$_SESSION[User::SESSION]["iduser"] > 0
+            ||
+            (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
+        )
+        {
+
+            header("Location: /admin/login");
+            exit;
+
+        }
+
+    }
+
+    public static function logout()
+    {
+
+        $_SESSION[User::SESSION] = NULL;
+
+    }
+
+}
+
+?>
--- views-cache/footer.065259dc4eaedc730787af8f7b93f245.rtpl.php
+++ views-cache/footer.065259dc4eaedc730787af8f7b93f245.rtpl.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        
+
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2018 </strong> Projeto Simonetti.
